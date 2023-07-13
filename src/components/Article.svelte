5b7cd6e9db9523a247ace7cf4651c6a9fbf166d1
Diamant Lelouette <git@diamant.dev>
2023-07-14 00:46:27 +0200

    Language fixes !

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -7,6 +7,22 @@
   export let description: string;
   export let type: string;
 
+  moment.locale('fr', {
+    relativeTime: {
+      past: 'il y a %s',
+      s: 'quelques secondes',
+      m: 'une minute',
+      mm: '%d minutes',
+      h: 'une heure',
+      hh: '%d heures',
+      d: 'un jour',
+      dd: '%d jours',
+      M: 'un mois',
+      MM: '%d mois',
+      y: 'un an',
+      yy: '%d ans',
+    },
+  });
   moment.locale(url.split('/')[0]);
   $: strDate = moment(date).fromNow();
 </script>
